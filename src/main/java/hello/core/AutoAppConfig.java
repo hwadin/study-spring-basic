ea819273e731ef5cbb28e014e1435bedba694ad0
namu6747 <namu6747@naver.com>
2022-07-02 17:27:46 +0900

    2022-07-02 T17:27:00

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,12 @@
 package hello.core;
 
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -16,6 +23,19 @@ import org.springframework.context.annotation.FilterType;
 
 public class AutoAppConfig {
 
+        @Autowired
+        MemberRepository memberRepository;
+        @Autowired
+        DiscountPolicy discountPolicy;
 
+//        @Bean
+        OrderService orderService(){
+                return new OrderServiceImpl(memberRepository, discountPolicy);
+        }
+
+//        @Bean
+        MemberRepository memberRepository(){
+                return new MemoryMemberRepository();
+        }
 
 }
